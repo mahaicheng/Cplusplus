eee1e4536779dded52cb1626064c943712f196c4
Ma Haicheng <hchma@outlook.com>
2016-05-16 10:13:08 +0800

    add setTest.cpp

---
+++ setTest.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> 文件名: setTest.cpp
+	> 作者: 马海城
+	> 邮箱: hchma@outlook.com
+	> 创建日期: 2016年05月16日 星期一 09时38分33秒
+ ************************************************************************/
+
+#include<iostream>
+#include<set>
+#include<algorithm>
+#include<iterator>
+
+using namespace std;
+
+int main()
+{
+    set<int> iSet{8,7,9,3,1,2,6,5,4,0};
+    copy(iSet.begin(), iSet.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    auto iter = lower_bound(iSet.begin(), iSet.end(), 4);
+
+    int tmpseqno = -1;
+    if (iter != iSet.end())
+    {
+        iSet.erase(iSet.begin(), iter);
+        tmpseqno = *iter;
+    }
+    copy(iSet.begin(), iSet.end(), ostream_iterator<int>(cout, " "));
+    cout << "\ntmpseqno = " << tmpseqno << endl;
+}
