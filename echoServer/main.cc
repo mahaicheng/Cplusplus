eff02da19729fc00f60ee7e3d833d92cebd84fc2
Ma Haicheng <hchma@outlook.com>
2016-05-24 21:20:55 +0800

    delete zhushi

--- echoServer/main.cc
+++ echoServer/main.cc
@@ -1,11 +1,4 @@
-﻿/*************************************************************************
-	> 文件名: main.cc
-	> 作者: 马海城
-	> 邮箱: hchma@outlook.com
-	> 创建日期: 2016年05月24日 星期二 16时22分08秒
- ************************************************************************/
-
-#include"EchoServer.h"
+﻿#include"EchoServer.h"
 #include<muduo/net/EventLoop.h>
 #include<muduo/net/InetAddress.h>
 
